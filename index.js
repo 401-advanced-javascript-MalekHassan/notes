ebdb23f9e12bbef595c619b8f1049fa24a989d31
MalekHassan <malekdalalha1995@gmail.com>
2020-10-14 01:24:06 +0300

    doing lab03 with out the DB

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
-
+require('dotenv').config('.env');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
+const mongoose = require('mongoose');
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/notes';
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+  })
 const myInputs = new Input();
 const newNotes = new Notes();
 // console.log(options)
-newNotes.execute(myInputs);
+newNotes.execute(myInputs).then(mongoose.disconnect);
