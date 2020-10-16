9d56160c08fd40c4e07b18339d33a73016545448
MalekHassan <malekdalalha1995@gmail.com>
2020-10-17 00:44:31 +0300

    fixing some errors

--- __tests__/collection.test.js
+++ __tests__/collection.test.js
@@ -1,46 +1,30 @@
-'use strict';
-require('@code-fellows/supergoose');
-const collection = require('../lib/collection.js');
-const newColliction = new collection()
-jest.spyOn(global.console, 'log');
+"use strict";
+require("@code-fellows/supergoose");
+const collection = require("../lib/collection.js");
+const newColliction = new collection();
+jest.spyOn(global.console, "log");
 
-describe('saving Module', ()=> {
-    it('vaild saveing method case', ()=> {
-       let obj = {action:'add',payload: 'fffffff', category: 'malek' };
-       return newColliction.saveing(obj).then((record) => {
-        Object.keys(obj).forEach((key) => {
-            expect(record[key]).toEqual(obj[key])
-        })
-    })
+describe("collection module  testing", () => {
+  it("vaild saveing method case", () => {
+    let obj = { action: "add", payload: "fffffff", category: "malek" };
+    return newColliction.saveing(obj).then((record) => {
+      Object.keys(obj).forEach((key) => {
+        expect(record[key]).toEqual(obj[key]);
+      });
     });
-   it('vaild list method case', async()=> {
-        let obj = { category: 'greating' };
-        await newColliction.list(obj).then((record) => {
-            console.log(record, 'recoed');
-            record.forEach((item) => {
-             expect(item[category]).toEqual(obj[category])
-            })
-        })
+  });
+  it("vaild list method case", async () => {
+    let obj = { category: "greating" };
+    await newColliction.list(obj).then((record) => {
+      console.log(record, "recoed");
+      record.forEach((item) => {
+        expect(item[category]).toEqual(obj[category]);
+      });
     });
-     it('deleteFunction list method case', ()=> {
-        let num = 11111111;
-        newColliction.deleteFunction(num)
-        
-             expect(num).toEqual(num)
-            })
-       
-    });
-
-    // it('vaild list method case', ()=> {
-    //     expect(console.log).toHaveBeenCalled();
-    //    });
-    //    it('Invaild list method case', ()=> {
-    //      expect(newColliction.list()).toEqual({});
-    //    });
-
-    //    it('vaild saveing method case', ()=> {
-    //     expect(console.log).toHaveBeenCalled();
-    //    });
-    //    it('Invaild saveing method case', ()=> {
-    //      expect(newColliction.saveing({action:'s',payload: 'fffffff', category: 'malek' })).toEqual({});
-    //    });
+  });
+  it("valid entered data for deleteFunction method case", async () => {
+    let num = '5f87379274bf461f12bb42dd';
+    await newColliction.deleteFunction(num);
+    expect(num).toEqual(num);
+  });
+});
