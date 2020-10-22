b1489b6f3ce9cc673d747490774e070b02720dcb
MalekHassan <malekdalalha1995@gmail.com>
2020-10-22 15:06:20 +0300

    fixing the lint errors

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,21 +1,22 @@
 'use strict';
 
-
 const Notes = require('../lib/notes.js');
 const notes = new Notes();
 
 jest.spyOn(global.console, 'log');
 
-describe('Notes Module', ()=> {
-  it('execute() does nothing with invalid notes', ()=> {
-    notes.execute({action:'s', payload:'ffff'});
+describe('Notes Module', () => {
+  it('execute() does nothing with invalid notes', () => {
+    notes.execute({ action: 's', payload: 'ffff' });
     expect(console.log).toHaveBeenCalled();
   });
-    
-  it('exexute() will log out notes when given', ()=> {
-    expect(notes.execute({action:'add',payload:'Hello'})).toEqual( notes.execute({action:'add',payload:'Hello'}));
+
+  it('exexute() will log out notes when given', () => {
+    expect(notes.execute({ action: 'add', payload: 'Hello' })).toEqual(
+      notes.execute({ action: 'add', payload: 'Hello' })
+    );
   });
-  it('exexute() will log out notes when given', ()=> {
+  it('exexute() will log out notes when given', () => {
     expect(console.log).toHaveBeenCalled();
   });
 });
--- index.js
+++ index.js
@@ -3,18 +3,20 @@ require('dotenv').config('.env');
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 const mongoose = require('mongoose');
-const MONGODB_URI =  process.env.MONGODB_URI || 'mongodb://localhost:27017/notes';
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/notes';
 
 mongoose
-.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}).catch((err) => {
-  console.log('NOOOOOOOOOOOOOOOOOOOOOOOOOOO', err.message);
-})
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => {
+    console.log('NOOOOOOOOOOOOOOOOOOOOOOOOOOO', err.message);
+  });
 const myInputs = new Input();
 const newNotes = new Notes();
 // console.log(options)
-newNotes.execute(myInputs) 
+newNotes.execute(myInputs);
